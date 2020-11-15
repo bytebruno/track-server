bd276355b73aa9b1981843aad83638c3a10a1c1f
Bruno Rodrigues <bytebruno@gmail.com>
2020-11-15 16:40:56 -0300

    feat: Created Track routes file and get tracks method
    Now, the users can get their tracks from the mongodb database

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 require('./models/User');
+require('./models/Track');
 
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://bytebruno:159753@cluster0.9yait.mongodb.net/<dbname>?retryWrites=true&w=majority";
